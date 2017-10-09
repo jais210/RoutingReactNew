09dba8d515ace364113d3b67b73e720beae25bee
jais210 <jakeline_210@hotmail.com>
2017-10-09 17:47:12 -0500

    img-teachers

--- src/App.js
+++ src/App.js
@@ -14,37 +14,37 @@ import {
 // arreglo de profesores
 const teacherList = [
   {
-    img: "img/angieMcAngular.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/angie.png",
     name: "Angie McAngular",
     information:
     "Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!"
   },
   {
-    img: "img/geoLoCation.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/geo.png",
     name: "Gelo 'Lo' Cation",
     information:
     "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code."
   },
   {
-    img: "img/jayQuery.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/jay.png",
     name: "Jay Query",
     information:
     "Jay is a developer, author of CSS: The Missing Manual, JavaScript & jQuery: The Missing Manual, and web development teacher."
   },
   {
-    img: "img/nodeStradamus.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/nodestradamus.png",
     name: "NodeStradamus",
     information:
     "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps."
   },
   {
-    img: "img/ecmaScriptnstuff.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/ecma.png",
     name: "Ecma Scriptnstuff",
     information:
     "Ecma found her passion for computers and programming over 15 years ago. She is excited to introduce people to the wonderful world of JavaScript."
   },
   {
-    img: "img/jsonBabel.png",
+    img: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/json.png",
     name: "Json Babel",
     information:
     "All of his professional life, Json has worked with computers online; he is a polyglot programmer and likes using the right tools for the job."
@@ -317,7 +317,7 @@ const NotFoundPage = (props) => {
 }
 
 const App = () => (
-   <div className= "content">
+   <div className= 'content'>
   <BrowserRouter>
    
       <header>
